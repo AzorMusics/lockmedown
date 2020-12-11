ec5dbd38b95be95df51d49f936774c5756d8ff5a
Azor <60231307+AzorMusics@users.noreply.github.com>
2020-12-11 21:34:13 +0100

    Add files via upload

---
+++ LockMeDown/ByteManager.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace LockMeDown
+{
+    class ByteManager
+    {
+
+        public static byte[] GetPasswordBytes(string password)
+        {
+            byte[] e = System.Text.Encoding.UTF8.GetBytes(password);
+
+            return e;
+        }
+
+        public static void LockFile(string filename, byte[] passwordBytes)
+        {
+            byte[] eBytes = File.ReadAllBytes(filename);
+
+            ulong[] afterwork = new ulong[eBytes.Length];
+
+            Console.WriteLine("Locking File");
+            Console.WriteLine();
+            int n = 0;
+            int compn = 0;
+            foreach(byte p in eBytes)
+            {
+                if(compn == passwordBytes.Length)
+                {
+                    compn = 0;
+                }
+
+                afterwork[n] = Convert.ToUInt64(eBytes[n] * passwordBytes[compn]);
+
+                compn++;
+                n++;
+            }
+
+            using (StreamWriter writer = new StreamWriter(filename + ".lmd"))
+            {
+                foreach(ulong e in afterwork)
+                {
+                    writer.Write(e + " ");
+                }
+
+                writer.Close();
+            }
+
+            Console.WriteLine("File locked with your Password!");
+            Console.WriteLine();
+        }
+
+        public static void UnlockFile(string filename, byte[] passwordBytes)
+        {
+            string fullfilecontent = File.ReadAllText(filename);
+
+            string[] fileparts = fullfilecontent.Split(' ');
+
+            ulong[] lockedFileBytes = new ulong[fileparts.Length - 1];
+
+            int pn = 0;
+
+            Console.WriteLine("Unlocking File");
+            Console.WriteLine();
+            Console.WriteLine("Step 1: Reading locked bytes");
+
+            foreach (string e in fileparts)
+            {
+                try
+                {
+                    ulong e2 = Convert.ToUInt64(e);
+                    lockedFileBytes[pn] = e2;
+
+                }
+                catch
+                {
+
+                }
+
+                pn++;
+            }
+
+
+            Console.WriteLine($"{lockedFileBytes.Length} locked bytes found.");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Step 2: Unlocking the locked bytes");
+
+            byte[] afterwork = new byte[lockedFileBytes.Length];
+
+            
+            int n = 0;
+            int compn = 0;
+            foreach (ulong p in lockedFileBytes)
+            {
+                if (compn == passwordBytes.Length)
+                {
+                    compn = 0;
+                }
+
+                afterwork[n] = Convert.ToByte(lockedFileBytes[n] / passwordBytes[compn]);
+
+                compn++;
+                n++;
+            }
+
+            Console.WriteLine("The locked bytes got unlocked. If you didnt put the right password, the file output will be broken and you wont be able to use the file properly.");
+
+            string extension = ".lmd";
+
+            string result = filename.Substring(0, filename.Length - extension.Length);
+
+            File.WriteAllBytes(result, afterwork);
+
+            Console.WriteLine("File unlocked with the Password you put in!");
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ LockMeDown/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+
+namespace LockMeDown
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if(args.Length == 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("You need to enter 'lockmedown --help' for all commands.");
+                Console.WriteLine();
+            }
+            else
+            {
+                if(args[0] == "--help")
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("lockmedown --lock <password> <file path>       | It encrypts a File");
+                    Console.WriteLine("lockmedown --unlock <password> <file path>     | It decrypts a File");
+                    Console.WriteLine("lockmedown --bytecount <file path>             | It counts all bytes in a File");
+                    Console.WriteLine();
+                }
+                else if(args[0] == "--lock")
+                {
+                    if(args[1].Length != 0)
+                    {
+                        byte[] e = ByteManager.GetPasswordBytes(args[1]);
+
+                        if(File.Exists(args[2]))
+                            ByteManager.LockFile(args[2], e);
+                        else
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("File is not available.");
+                            Console.WriteLine();
+                            return;
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Password Invalid.");
+                        Console.WriteLine();
+                    }
+                }
+                else if (args[0] == "--unlock")
+                {
+                    if (args[1].Length != 0)
+                    {
+                        byte[] e = ByteManager.GetPasswordBytes(args[1]);
+                        if (File.Exists(args[2]))
+                            ByteManager.UnlockFile(args[2], e);
+                        else
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("File is not available.");
+                            Console.WriteLine();
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Please use a password to unlock.");
+                        Console.WriteLine();
+                    }
+                }
+                else if (args[0] == "--bytecount")
+                {
+                    if (args[1].Length != 0)
+                    {
+                        if(File.Exists(args[1]))
+                        {
+                            byte[] filebytes = File.ReadAllBytes(args[1]);
+
+                            Console.WriteLine();
+                            Console.WriteLine("This File has " + filebytes.Length + " Bytes");
+                            Console.WriteLine();
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Please give me a actual File Path.");
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
